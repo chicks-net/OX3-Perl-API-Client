42311e16df8c87e6acfcaed8a7fc87f07de65a31
chris.hicks <chris.hicks@505b5b21-9b2e-481b-9bd2-5f23bc09c1f5>
2011-08-18 23:32:12 +0000

    -skeleton for module
    git-svn-id: https://svn-priv.openx.org/svn/trunk/developers/chicks/ox3/oauth/OX-OAuth@19347 505b5b21-9b2e-481b-9bd2-5f23bc09c1f5

---
+++ lib/OX/OAuth.pm
@@ -0,0 +1,110 @@
+package OX::OAuth;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+OX::OAuth - The great new OX::OAuth!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use OX::OAuth;
+
+    my $foo = OX::OAuth->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Christopher Hicks, C<< <chris.hicks at openx.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-ox-oauth at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=OX-OAuth>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc OX::OAuth
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=OX-OAuth>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/OX-OAuth>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/OX-OAuth>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/OX-OAuth/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011 Christopher Hicks.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of OX::OAuth
