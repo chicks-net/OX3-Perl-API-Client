0c336827c722940162aff1966e83647e9e8d2b33
chris.hicks <chris.hicks@505b5b21-9b2e-481b-9bd2-5f23bc09c1f5>
2011-08-30 22:37:24 +0000

    v0.51: - add validate to login() - put the cookie header in so things actually work
    git-svn-id: https://svn-priv.openx.org/svn/trunk/developers/chicks/ox3/oauth/OX-OAuth@19912 505b5b21-9b2e-481b-9bd2-5f23bc09c1f5

--- lib/OX/OAuth.pm
+++ lib/OX/OAuth.pm
@@ -19,11 +19,11 @@ OX::OAuth - use OpenX's OAuth login mechanism
 
 =head1 VERSION
 
-Version 0.50
+Version 0.51
 
 =cut
 
-our $VERSION = '0.50';
+our $VERSION = '0.51';
 
 
 =head1 SYNOPSIS
@@ -266,6 +266,18 @@ sub login {
 	}
 
 	$self->{token} = $oauth_access_token;
+	$ua->default_header('Cookie' => "openx3_access_token=$oauth_access_token"); # the normal cookie jar uses a format that breaks something
+
+	#
+	# validate with ox3 api, almost done
+	#
+
+	$self->rest({
+		relative_url => '/a/session/validate',
+		success => 'login validated...',
+		decode_json => 0,
+		method => 'PUT',
+	});
 
 	return 1; # woo hoo, success!
 }
@@ -335,6 +347,7 @@ sub rest {
 
 	# reflect on self
 	my $ua = $self->{_ua};
+#	my $token = $self->{token};
 
 	# read arguments
 	my $url;
@@ -350,6 +363,7 @@ sub rest {
 
 	my $success = $args->{success} || "$url succeeded";
 	my $post_args = $args->{post_args};
+	my $method = $args->{method};
 	my $debug = $args->{debug} || 0;
 
 	my $decode_json = 1;
@@ -371,6 +385,10 @@ sub rest {
 				$upload_file_field => [ $upload_file ],
 			]
 		);
+	} elsif (defined $method) {
+		if ($method eq 'PUT') {
+			$response = $ua->request(PUT $url);
+		} # TODO: more options?
 	} elsif (defined $post_args) {
 		$response = $ua->post($url,$post_args);
 	} else {
@@ -479,523 +497,8 @@ See http://dev.perl.org/licenses/ for more information.
 1; # End of OX::OAuth
 
 __END__
-#!/usr/bin/perl
-
-use warnings;
-use strict;
-
-use LWP::UserAgent;
-use Data::Dumper;
-use JSON -support_by_pp;
-use File::Slurp;
-use Digest::SHA1 qw(sha1_hex sha1_base64);
-
-# config
-my $base_url='http://prod-ox3-ui-xv-06.xv.dc.openx.org/ox/3.0';
-my $base_url_secure='https://prod-console.openx.com/ox/3.0'; # can't use it for now anyway
-my $auth = {
-	email => 'root@openx.org',
-	password => '279b62d07f117bf50e12a84d9d4e7063',
-};
-my $sso_auth = {
-	email => 'openx-api@openx.org',
-	pass => 'ajN58Y0Gux2',
-};
-my $environment = 'production';
-my $iab486x60 = 8;
-my $creative_path = '/home/chicks/Downloads/openx-468x60.png';
-my $ua_timeout = 10; # seconds
-
-# setup UA
-my $ua = LWP::UserAgent->new;
-$ua->timeout($ua_timeout); # configured above
-$ua->env_proxy;
-$ua->cookie_jar( { file => "$ENV{HOME}/.ox3.cookies.txt" } );
-$ua->agent('ox3-customer-creator/0.3');
-
-# load paramaters from file
-my ($shortname) = @ARGV;
-die "provide shortname or shortname.json as argument please" unless defined $shortname and length $shortname;
-
-my $configfile;
-
-if ($shortname =~ /\.json$/) {
-	$configfile = $shortname;
-	$shortname =~ s/\.json$//;
-} else {
-	$configfile = "$shortname.json";
-}
-
-unless (-f $configfile) {
-	die "no config file $configfile";
-}
-
-my $config = jsondecode(read_file($configfile));
-#jsondump($config); die;
-my $masternetworkid = $config->{masternetworkid};
-my $timezone = $config->{timezone};
-my $uihostname = $config->{ui};
-my $deliveryhostname = $config->{delivery};
-my $imagehostname = $config->{image};
-my $deliverysslhostname = $config->{deliveryssl};
-my $content_topic_origin = $config->{content_topic_origin} || 'Custom'; # we never use Master anymore
-my $country = $config->{country} || 'us';
-my $currency = $config->{currency} || 1; # default = USD
-my $ssorealm = "${shortname}_ad_server";
-my $apihostname = "$shortname-ui3.openxenterprise.com";
-
-# get a session
-rest({
-	url => "$base_url/a/session/",
-	success => 'logged in...',
-	post_args => $auth,
-});
-
-#goto upload_creative;
-#goto create_ad;
-
-# create master network
-unless (defined $masternetworkid) {
-	my $mna_args = {
-		name => "$shortname Instance",
-		status => "Active",
-		account_id => "1",
-		account_type_id => "1",
-		currency_id => $currency,
-		timezone_id => $timezone,
-		country_of_business_id => $country,
-	};
-	#print Dumper($instanceargs); die;
-
-	my $mna = rest({
-		url => "$base_url/a/account",
-		success => 'created master network account',
-		post_args => $mna_args,
-	});
-	$masternetworkid = $mna->{id};
-}
-
-# get master network
-my $masternetwork = rest({
-	url => "$base_url/a/account/$masternetworkid",
-	success => "got master network",
-});
-
-my $instanceid;
-unless ($masternetwork->{instance_id} and $masternetwork->{instance_id} != 1) {
-	#my $secret = 'Zombies!!!';
-	my $secret = sha1_hex(rand(100_000_000_000_000));
-	my $consumer_key = sha1_hex(rand(100_000_000_000_000));
-	my $consumer_secret = sha1_hex($secret);
-
-	# create instance
-	my $instanceargs = {
-		name => $shortname,
-		timezone_id => $timezone,
-		sso_realm => $ssorealm,
-		sso_domain => 'https://sso.openx.com',
-		environment => $environment,
-		delivery_hostname => $deliveryhostname,
-		admin_hostname => $uihostname,
-		api_hostname => $apihostname,
-		cdn_hostname => $imagehostname,
-		cdn_ssl_hostname => 'ssl-i.cdn.openx.com',
-		content_topic_origin => $content_topic_origin,
-		active => 1,
-		account_id => $masternetworkid,
-		consumer_key => $consumer_key,
-		consumer_secret => $consumer_secret,
-		theme => 'oxtongue',
-		market_operator_id => 'OX',
-	};
-	#print Dumper($instanceargs); die;
-
-	my $ci = rest({
-		url => "$base_url/a/instance",
-		success => 'created instance',
-		post_args => $instanceargs,
-	});
-	$instanceid = $ci->{id};
-
-#	# update master network
-#	my $masterupdate = {
-#		instance_id => $instanceid,
-#		name => $masternetwork->{name}, # unchanged but required
-#		status => $masternetwork->{status}, # unchanged but required
-#		timezone_id => $masternetwork->{timezone_id}, # unchanged but required
-#	};
-#
-#	my $mu = rest ({
-#		url => "$base_url/a/account/$masternetworkid",
-#		success => 'updated master instance',
-#		post_args => $masterupdate,
-#	});
-
-	# create sso realm
-	my $sso_realm_args = {
-		%$sso_auth,
-		realm		=>	$ssorealm,		# realm - unique realm identifier.
-								# Needs to match against ^[a-zA-Z0-9_]+$
-
-		name		=>	$shortname,		# name - name of the SSO Realm.
-
-		# secret - shared secret for the SSO realm. Needs to match against /^[a-zA-Z0-9_!@#]+$/
-		# and must match the existing API shared secret if the realm is for the API. Check
-		# config.php in the API to get this information.
-	#	secret		=>	'test', # qa
-		secret		=>	$secret, # prod
-		consumer_key	=>	$consumer_key,
-
-		domain		=>	'sso.openx.com',	# domain - base domain for the realm. This is
-								# used for skinning, which we haven't started
-								# using yet. Needs to match against
-								# ^[a-zA-Z0-9.-]+$ . In production this is
-								# 'sso.openx.com'. 
-	};
-	my $sso_realm = rest({
-		url => "https://sso.openx.com/api/supervisor/createrealm",
-		success => "created sso realm",
-		post_args => $sso_realm_args,
-		decode_json => 0,
-	});
-
-	if ($content_topic_origin eq 'Custom') {
-		my $ct_args = {
-			id => "9999",
-			name => "Unclassified",
-			instance_id => $instanceid,
-			parent_id => "0",
-		};
-		#print Dumper($instanceargs); die;
-
-		my $ct = rest({
-			url => "$base_url/a/contenttopic",
-			success => 'created content topic "Unclassified"',
-			post_args => $ct_args,
-		});
-	}
-
-} else { # unless ($masternetwork->{instance_id}) 
-	print "master network account $masternetworkid already has instance id " . $masternetwork->{instance_id} . "\n";
-}
-
-#die "skip the rest because of random funkiness";
-
-# create publisher account
-#
-# POST /a/account/
-#
-# required fields:
-# 	name			= Monitoring Publisher (don't use)
-# 	status			= Active
-# 	account_id		= ???
-# 	account_type_id		= 2 (Publisher: Exclusive)
-# 	currency_id		= 1 (USD)
-# 	timezone_id		= 8 (PST)
-# 	country_of_business_id	= 'us'
-
-my $publisher_args = {
-	name		=> "Monitoring Publisher (don't use)",
-	status		=> "Active",
-	account_id	=> $masternetwork->{id},
-	account_type_id => 2, # Publisher: Exclusive
-	currency_id	=> 1, # USD
-	timezone_id	=> 8, # PST
-	country_of_business_id => 'us',
-};
-
-my $publisher = rest({
-	url => "$base_url/a/account/",
-	success => "created Monitoring publisher account",
-	post_args => $publisher_args,
-});
-
-# create "Default" publisher account
-my $default_publisher_args = {
-	name		=> "Default Publisher",
-	status		=> "Active",
-	account_id	=> $masternetwork->{id},
-	account_type_id => 2, # Publisher: Exclusive
-	currency_id	=> $currency,
-	timezone_id	=> $timezone, # their timezone
-	country_of_business_id => $country,
-};
-
-my $default_publisher = rest({
-	url => "$base_url/a/account/",
-	success => "created Default publisher account",
-	post_args => $default_publisher_args,
-});
-
-# create advertiser account
-#
-# POST /a/account/
-#
-# required fields:
-# 	name			= Monitoring Advertiser (don't use)
-# 	status			= Active
-# 	account_id		= ???
-# 	account_type_id		= 4 (Advertiser: Managed)
-# 	currency_id		= 1 (USD)
-# 	timezone_id		= 8 (PST)
-# 	country_of_business_id	= 'us'
-
-my $advertiser_args = {
-	name		=> "Monitoring Advertiser (don't use)",
-	status		=> "Active",
-	account_id	=> $masternetwork->{id},
-	account_type_id => 4, # Advertiser: Managed
-	currency_id	=> 1, # USD
-	timezone_id	=> 8, # PST
-	country_of_business_id => 'us',
-};
-
-my $advertiser = rest({
-	url => "$base_url/a/account/",
-	success => "created advertiser account",
-	post_args => $advertiser_args,
-});
-
-# create site
-#
-# POST /a/site/
-#
-# required fields:
-# 	name			= Monitoring Site (don't use)
-# 	status			= Active
-# 	url			= http://www.openx.org
-# 	content_topic_id	= 9999 (Unclassified)
-# 	content_type_id		= 99 (Unclassified)
-# 	account_id		= $ (publisher account id)
-
-my $site_args = {
- 	name			=> "Monitoring Site (don't use)",
- 	status			=> "Active",
- 	url			=> "http://www.openx.org",
- 	content_topic_id	=> 9999, # (Unclassified)
- 	content_type_id		=> 99, # (Unclassified)
- 	account_id		=> $publisher->{id} #publisher account id
-};
-my $site = rest({
-	url => "$base_url/a/site/",
-	success => "created site",
-	post_args => $site_args,
-});
-
-# create adunit
-#
-# POST /a/adunit/
-#
-# required fields:
-# 	name			= Monitoring Ad Unit (don't use)
-# 	status			= Active
-# 	site_id			= $ (site just created)
-# 	delivery_medium_id	= 2 (web)
-#	size_id			= 8
-#	tag_type_id		= 1
-
-my $adunit_args = {
- 	name			=> "Monitoring Ad Unit (don't use)",
- 	status			=> "Active",
- 	site_id			=> $site->{id},
- 	delivery_medium_id	=> 2, # web
- 	size_id			=> $iab486x60, # IAB Full Banner 468x60
-	tag_type_id		=> 1 # javascript synchronous,
-};
-my $adunit = rest({
-	url => "$base_url/a/adunit/",
-	success => "created adunit",
-	post_args => $adunit_args,
-});
-
-# associate content topic with ad unit
-#
-# GET /a/adunit/${ad_unit_id}/associateContentTopic/9999 	
-my $adunit_id = $adunit->{id};
-rest({
-	url => "$base_url/a/adunit/$adunit_id/associateContentTopic/9999",
-	success => "associated content topic 9999 with adunit $adunit_id",
-});
-
-# create order
-#
-# POST /a/order/
-#
-# required fields:
-# 	name			= Monitoring Order (don't use)
-# 	status			= Pending (??)
-# 	start_date		= now in yyyy-mm-dd
-# 	end_date		= null
-#	account_id		= $ (advertiser account id)
-
-my $order_args = {
- 	name			=> "Monitoring Order (don't use)",
- 	status			=> "Pending",
-	start_date		=> now(),
-	end_date		=> 'null', # TODO: null properly?
- 	account_id		=> $advertiser->{id} # advertiser account id
-};
-my $order = rest({
-	url => "$base_url/a/order/",
-	success => "created order",
-	post_args => $order_args,
-});
-
-# create lineitem
-#
-# POST /a/lineitem/
-#
-# required fields:
-# 	name			= Monitoring Line Item (don't use)
-# 	status			= Pending (??)
-# 	buying_model_id		= 5 (house)
-# 	delivery_medium_id	= 2 (web)
-# 	start_date		= now in yyyy-mm-dd
-# 	order_id		= $ (order id just created)
-#	end_date		= null
-
-my $lineitem_args = {
- 	name			=> "Monitoring Line Item (don't use)",
- 	status			=> "Pending",
- 	buying_model_id		=> 5, # house
- 	delivery_medium_id	=> 2, # web
-	start_date		=> now(),
- 	order_id		=> $order->{id}, # order id for order just created
-	end_date		=> 'null',
-};
-my $lineitem = rest({
-	url => "$base_url/a/lineitem/",
-	success => "created lineitem",
-	post_args => $lineitem_args,
-});
-
-# create rules
-#
-# POST /a/rule/
-#
-# required fields:
-# 	dimension		= content
-# 	attribute		= adunit_size
-# 	operator		= INTERSECTS
-# 	value			= 8 (IAB Full Banner 468x60)
-# 	lineitem_id		= $ (lineitem id just created)
-
-my $rule1_args = {
- 	dimension		=> 'content',
- 	attribute		=> 'adunit_size',
- 	operator		=> 'INTERSECTS',
- 	value			=> $iab486x60, # IAB Full Banner 468x60
- 	lineitem_id		=> $lineitem->{id}, # lineitem id for lineitem just created
-};
-my $rule1 = rest({
-	url => "$base_url/a/rule/",
-	success => "created rule1",
-	post_args => $rule1_args,
-});
-
-# required fields:
-# 	dimension		= dimension
-# 	attribute		= _operator
-# 	operator		= AND
-# 	value			= NO_VALUE
-# 	lineitem_id		= $ (lineitem id just created)
-#
-# I'm not sure what the second one does
-
-my $rule2_args = {
- 	dimension		=> 'dimension',
- 	attribute		=> '_operator',
- 	operator		=> 'AND',
- 	value			=> 'NO_VALUE', # IAB Full Banner 468x60
- 	lineitem_id		=> $lineitem->{id}, # lineitem id for lineitem just created
-};
-my $rule2 = rest({
-	url => "$base_url/a/rule/",
-	success => "created rule2",
-	post_args => $rule2_args,
-});
-
-# there was a "POST /a/ad/TypeLimiter" but it doesn't seem to create anything. 
-# And it doesn't return anything different regardless of the values passed in.
-
-# upload creative
-#
-# POST /a/creative/uploadCreative
-#
-# required fields:
-# 	name			= Monitoring Ad Creative (don't use)
-# 	account_id		= ??? (advertiser account id)
-# 	userfile		= binary file in multipart with filename
-upload_creative:
-#$advertiser = { id => 9091 };
-#$lineitem = { id => 43033 };
-
-my $creative_upload_args = {
- 	name			=> "Monitoring Ad Creative (don't use)",
- 	account_id		=> $advertiser->{id} # advertiser account id
-};
-my $creative_upload = rest({
-	url => "$base_url/a/creative/uploadCreative",
-	success => "uploaded creative",
-	post_args => $creative_upload_args,
-	upload_file_field => 'userfile',
-	upload_file => $creative_path,
-});
-
-# create creative
-#
-# POST /a/creative/
-#
-# required fields:
-# 	name			= Monitoring Ad Creative (don't use)
-# 	ad_type_id		= 1
-# 	uri			= from creative upload
-# 	account_id		= $ (advertiser account id)
-
-my $creative_args = {
- 	name			=> "Monitoring Ad Creative (don't use)",
- 	ad_type_id		=> 1,
-	uri			=> $creative_upload->{uri},
- 	account_id		=> $advertiser->{id} # advertiser account id
-};
-my $creative = rest({
-	url => "$base_url/a/creative/",
-	success => "created creative",
-	post_args => $creative_args,
-});
-
-create_ad:
-#my $lineitem = {id => 19899};
-
-# create ad
-#
-# POST /a/ad/
-#
-# required fields:
-# 	name			= Monitoring Ad
-# 	status			= Active
-# 	ad_type_id		= 1
-# 	lineitem_id		= $ (line item id from above)
-
-my $ad_args = {
- 	name			=> "Monitoring Ad (don't use)",
- 	status			=> "Active",
- 	ad_type_id		=> 1,
- 	lineitem_id		=> $lineitem->{id}, # lineitem id for lineitem just created
- 	size_id			=> $iab486x60, # IAB Full Banner 468x60
-	click_url		=> 'http://openx.org',
-	click_target_window	=> '_blank',
-};
-my $ad = rest({
-	url => "$base_url/a/ad/",
-	success => "created ad",
-	post_args => $ad_args,
-});
 
 
-## 
-## SUBroutines
-##
 
 # make a rest post or get call and deal with errors
 sub rest {
@@ -1004,8 +507,10 @@ sub rest {
 	# read arguments
 	my $url = $args->{url};
 	die "bad url: $url" unless $url =~ qr{^https?://};
-	my $success = $args->{success} || "$url succeeded";
+	my $success = $args->{success};
+	$success = "$url succeeded" unless defined $success;
 	my $post_args = $args->{post_args};
+	my $method = $args->{method};
 	my $debug = $args->{debug} || 0;
 
 	my $decode_json = 1;
@@ -1027,9 +532,15 @@ sub rest {
 				$upload_file_field => [ $upload_file ],
 			]
 		);
+	} elsif (defined $method) {
+		if ($method eq 'PUT') {
+			$response = $ua->request(PUT $url);
+		} # TODO: more options?
 	} elsif (defined $post_args) {
+#		warn "post $url";
 		$response = $ua->post($url,$post_args);
 	} else {
+#		warn "get $url";
 		$response = $ua->get($url);
 	}
 
@@ -1038,8 +549,11 @@ sub rest {
 		my $content = $response->content;
 
 		if ($debug) {
-			print "$success\n";
+			print "success would be: $success\n";
+			print "url: $url\n";
 			print "sent: " . Dumper($post_args) . "\n";
+			print "status line: " .  $response->status_line . "\n";
+			print "content: $content\n";
 			jsondump($content);
 			die "debug";
 		}
@@ -1055,7 +569,7 @@ sub rest {
 		if ( ref($href) eq 'HASH' and defined $href->{id} ) {
 			print "$success id " . $href->{id} . "\n";
 		} else {
-			print "$success\n";
+			print "$success\n" if length $success;
 		}
 		return $href;
 	} else {
@@ -1063,3 +577,18 @@ sub rest {
 	}
 }
 
+# decode JSON into perl structure
+sub jsondecode {
+	my ($content) = @_;
+#	print "### $content\n";
+	my $json = new JSON;
+	my $json_text = $json->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($content);
+	return $json_text;
+}
+
+# turn JSON into human readable output
+sub jsondump {
+	my($content) = @_;
+	my $json_text = jsondecode($content);
+	print Dumper($json_text);
+}
